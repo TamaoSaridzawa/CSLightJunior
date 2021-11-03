9f7c00f9ad4319b66429d3f0a62ab21b7101e1f6
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-04 00:38:23 +0300

    Контроль выхода

---
+++ OutputControl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String rightAnswer = "exit";
+            while (true)
+            {
+                Console.Write("Введите правильный ответ для выхода: ");
+                String userAnswer = Console.ReadLine();
+                if (rightAnswer == userAnswer)
+                {
+                    Console.WriteLine("Правильный ответ!");
+                    break;
+                }
+                else
+                    Console.WriteLine("Ответ не верный попробуйте еще раз!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
