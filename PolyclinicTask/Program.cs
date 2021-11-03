0cb57f29336b6075b3c82845e5179300656836f9
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-03 22:27:37 +0300

    Очередь в поликлиннике

---
+++ PolyclinicTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyclinicTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesPerPerson = 10;
+            Console.WriteLine($"Вас приветствует программа расчета времени ожидания в очереди.");
+            Console.Write($"Для рассчета времени ожидания введите количество людей в очереди: ");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int waitingTimeHours = minutesPerPerson * numberOfPeople / 60;
+            int waitingTimeMinutes = minutesPerPerson * numberOfPeople % 60;
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingTimeHours} часов и {waitingTimeMinutes} минут");
+            Console.ReadKey();
+        }
+    }
+}
