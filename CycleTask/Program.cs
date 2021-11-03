8f719f84fac06be35843577b7af4f4eadf51af7b
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-04 00:21:30 +0300

    Освоение циклов

---
+++ CycleTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 5;
+            Console.WriteLine("Введите сообщение");
+            String message = Console.ReadLine();
+            Console.Clear();
+            for (int i = 0; i < score; i++)
+            {
+                Console.WriteLine(message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
