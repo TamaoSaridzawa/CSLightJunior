d1a4045125aaec9833a9ff662a8f274e2df88837
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-03 22:42:13 +0300

    Работа со строками

---
+++ LinesTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут? : ");
+            String name = Console.ReadLine();
+            Console.Write("Сколько вам лет? : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Какой ваш любимый цвет? : ");
+            String color = Console.ReadLine();
+            Console.Write("В каком городе вы живете? : ");
+            String city = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {name}, ваш возраст - {age}, любимый цвет {color}, живете в городе {city}");
+            Console.ReadKey();
+        }
+    }
+}
