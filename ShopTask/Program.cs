7c6ba454d70687bdbeaf7bc0cd3c9fe57e4fec43
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-03 21:54:33 +0300

    Магазин кристаллов

---
+++ ShopTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceOfCrystals = 15;
+            Console.Write("Введите стартовое количество золота : ");
+            int gold = int.Parse(Console.ReadLine());
+            Console.Write($"Какое количество кристаллов вы хотите купить?" 
+                + $"  Цена за шт. = {priceOfCrystals} монет: ");
+            int numberOfCrystals = int.Parse(Console.ReadLine());
+            int remainingGold = gold - priceOfCrystals * numberOfCrystals;
+            Console.WriteLine($"Поздравляем с покупкой! : Счет кошелька : Кристаллы - {numberOfCrystals} . Золото - {remainingGold}");
+            Console.ReadLine();
+        }
+    }
+}
