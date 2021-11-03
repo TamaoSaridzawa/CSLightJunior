97700c96343141640663536ff43595249f1b694e
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-03 21:05:51 +0300

    Картинки

---
+++ ImageTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesQuantity = 52;
+            int picturesInaRow = 3;
+            int numbersOfRows = picturesQuantity / picturesInaRow;
+            int extraPictures = picturesQuantity % picturesInaRow;
+            Console.WriteLine($"Количесво полностью заполненных рядов = {numbersOfRows}, лишних картинок - {extraPictures}");
+            Console.ReadKey();
+        }
+    }
+}
