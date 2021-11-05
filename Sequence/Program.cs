95666b1e2e9a3d52e86baf06169263ff9edbcd50
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-06 01:07:28 +0300

    Последовательность

---
+++ Sequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 7;
+            int finalNumber = 98;
+            int step = 7;
+
+            for (int i = startNumber; i <= finalNumber; i += step)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
