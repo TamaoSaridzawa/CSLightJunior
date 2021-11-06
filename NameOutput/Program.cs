d462cd11c134ca091939b543921eb3272297105e
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-06 11:02:03 +0300

    Вывод имени в рамке

---
+++ NameOutput/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя :");
+            string name = Console.ReadLine();
+            Console.Write("Введите символ, из которого будет состоять рамка :");
+            string symbol = Console.ReadLine();
+
+            for (int column = 0; column < 3; column++)
+            {
+                for (int row = 0; row <= name.Length + 1; row++)
+                {
+                    if (column == 1)
+                    {
+                        Console.Write($"{symbol}{name}{symbol}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(symbol);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
