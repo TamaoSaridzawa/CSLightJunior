e82e9fc3288bd0dd1c16e1bab78eb917c5441df4
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-06 13:26:40 +0300

    Программа под паролем

---
+++ PasswordProgramProtected/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordProgramProtected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pasword = "unity";
+            int attempts = 0;
+            while (attempts < 3)
+            {
+                Console.Write("Введите пароль для получения секретного сообщения:");
+                string userInput = Console.ReadLine();
+
+                if (userInput == pasword)
+                {
+                    string secretMessage = "Никогда не сдавайся";
+                    Console.WriteLine($"Пароль верный!\n Секретное сообщение : {secretMessage}");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный пароль, попробуйте еще раз");
+                    attempts++;
+                }
+            }
+        }
+    }
+}
