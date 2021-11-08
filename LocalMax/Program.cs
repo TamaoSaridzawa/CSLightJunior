72cbeaa33ecb276d2451da6f01c3282b20f20006
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-08 14:48:51 +0300

    Локальные максимумы

---
+++ LocalMax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[30];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(10, 100);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
