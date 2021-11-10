96f5f000e9c22f7658732a7e6ab3e72024ae3aa9
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-10 23:40:24 +0300

    Конвертация через TryParse

---
+++ ReadInt/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ConvertString(out number);
+
+            Console.WriteLine($"Введенное число :{number}");
+
+            Console.ReadLine();
+        }
+
+        static int ConvertString(out int number)
+        {
+            number = 0;
+            bool success = false;
+
+            while (!success)
+            {
+                Console.Write("Введите число для конвертации :");
+                string userInput = Console.ReadLine();
+
+               success = int.TryParse(userInput, out number);
+
+                if (success)
+                {
+                    number = int.Parse(userInput);
+                    Console.WriteLine("Конвертация прошла успешно");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка конвертации, попробуйте еще раз");
+                }
+            }
+
+            return number;
+        }
+    }
+}
