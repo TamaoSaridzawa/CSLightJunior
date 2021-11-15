5b8a51055d2279038aa3f316854a7a301f556587
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-15 16:28:30 +0300

    Перемешать элементы массива

---
+++ ShuffleArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] array = { 2, 5, 1, 7, 9, 1, 4, 8 };
+
+            array = ShuffleArray(array, rand);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            
+            Console.ReadKey();
+        }
+
+        static int[] ShuffleArray(int[] array, Random rand)
+        {
+            int tempNumber;
+            int tempIndex;
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempIndex = rand.Next(i + 1);
+                tempNumber = array[tempIndex];
+                array[tempIndex] = array[i];
+                array[i] = tempNumber;
+            }
+
+            return array;
+        }
+    }
+}
